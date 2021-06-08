fd4f8c20a9998000135e02b7f844389705f27ebd
ZolF-werk <zolf@bk.ru>
2021-06-08 17:17:58 +0300

    1-homework

--- script.js
+++ script.js
@@ -20,17 +20,36 @@
 // const user = 'Ivan';
 // alert(`Hello, ${user}`);
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 // incr++;
 // decr--;
-console.log(incr++, --decr);
-console.log(5 % 2);
-console.log(2 + 2 * 4 !== 8);
+// console.log(incr++, --decr);
+// console.log(5 % 2);
+// console.log(2 + 2 * 4 !== 8);
 
-const isChecked = false,
-    isClosed = false;
-//   && - and
-//  ||  - or
-console.log(isChecked || !isClosed);
+// const isChecked = false,
+//     isClosed = false;
+// //   && - and
+// //  ||  - or
+// console.log(isChecked || !isClosed);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Последний из просмотренных фильмов', ''),
+    b = prompt('Оцените его по 10 шкале', ''),
+    c = prompt('Последний из просмотренных фильмов', ''),
+    d = prompt('Оцените его по 10 шкале', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
