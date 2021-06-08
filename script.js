21fedcf0cee98f4d20dbbda36d172da29c865eba
ZolF-werk <zolf@bk.ru>
2021-06-08 16:35:34 +0300

    5th try

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//alert('Hello');
+//const result = confirm('Are you sure?');
+//console.log(result);
+// const answer = +18prompt('Вам есть 18 лет?', 'ДА');
+// console.log(typeof answer);
+
+// const answers = [];
+// answers[0] = prompt('What is your name', '');
+// answers[1] = prompt('What is your second name', '');
+// answers[2] = prompt('How old are you', '');
+
+// document.write(answers);
+//****************************** */
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(incr++, --decr);
+console.log(5 % 2);
+console.log(2 + 2 * 4 !== 8);
+
+const isChecked = false,
+    isClosed = false;
+//   && - and
+//  ||  - or
+console.log(isChecked || !isClosed);
